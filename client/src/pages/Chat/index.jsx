163308ec4bb998477ee53eaaa3b33886b9bcfe2d
benwade91 <70988204+benn925@users.noreply.github.com>
2021-06-04 17:39:40 -0700

    added more features to frontend

--- client/src/pages/Chat/index.jsx
+++ client/src/pages/Chat/index.jsx
@@ -1,26 +1,65 @@
 import React, { useEffect, useState } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
+import ScrollToBottom from 'react-scroll-to-bottom';
 
 let socket;
 
 const Chat = ({ location }) => {
-    const [name, setName] = useState('')
-    const [room, setRoom] = useState('')
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const [message, setMessage] = useState([]);
+    const [messages, setMessages] = useState([]);
     const ENDPOINT = 'localhost:4001';
 
     useEffect(() => {
         const { name, room } = queryString.parse(location.search);
 
-        socket = io(ENDPOINT, {transports: ['websocket']})
+        socket = io(ENDPOINT)
 
         setName(name);
         setRoom(room);
-        
-        socket.emit('join', {name, room})
-        
-    },[ENDPOINT, location.search])
-    return <h1>Hello Chat</h1>
+
+        socket.emit('join', { name, room }, () => { })
+
+        return () => {
+            socket.emit('disconnect');
+            socket.off();
+        }
+
+    }, [ENDPOINT, location.search])
+
+    useEffect(() => {
+        socket.on('message', (message) => {
+            console.log(message);
+            setMessages([...messages, message])
+        })
+    }, []);
+
+    const sendMessage = (event) => {
+        event.preventDefault();
+
+        if (message) {
+            setMessages([...messages, message])
+            socket.emit('sendMessage', message, () => setMessage(''))
+        }
+    }
+
+    console.log(message, messages);
+
+    return <>
+        <div>
+        <ScrollToBottom>
+            <ul>
+            {messages.map(message => console.log(message))}
+            </ul>
+        </ScrollToBottom>
+            <input value={message}
+                onChange={event => setMessage(event.target.value)}
+                onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+            />
+        </div>
+    </>
 }
 
 export default Chat;
--- server/server.js
+++ server/server.js
@@ -9,8 +9,17 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
 app.use(routes);
+const httpProxy = require("http-proxy");
 
+httpProxy
+  .createProxyServer({
+    target: "http://localhost:3000",
+    ws: true,
+  })
+  .listen(80);
+  
 let interval;
 
 io.on("connection", (socket) => {
@@ -20,8 +29,27 @@ io.on("connection", (socket) => {
   }
   interval = setInterval(() => getApiAndEmit(socket), 1000);
 
-  socket.on('join', ({name, room}) => {
-    console.log(name,room);
+  socket.on("connect_error", (err) => {
+    console.log(`connect_error due to ${err.message}`);
+  });
+
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.emit('message', { user: 'admin', text: `Welcome to the chat ${user.name}` });
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined the chat!` })
+    socket.join(user.join);
+    callback();
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+    console.log(user, message);
+    io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
   })
 
   socket.on("disconnect", () => {
